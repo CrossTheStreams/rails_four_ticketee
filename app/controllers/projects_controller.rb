3f9e09e5f27d71250634cc0d90e62e40d2f59159
Andrew <arhautau@gmail.com>
2014-05-25 13:58:27 -0700

    handle bad request for projects#show. add before_action #set_project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+  before_action :set_project, :only => [:show, :edit, :update, :destroy]
+
   def index     
     @projects = Project.all
   end
@@ -9,7 +11,6 @@ class ProjectsController < ApplicationController
   end
 
   def create
-
     @project = Project.new(project_params)      
     if @project.save
       # reminder time : flash uses the session storage and will persist through redirection
@@ -23,15 +24,12 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id]) 
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
       redirect_to @project 
@@ -42,7 +40,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy 
-    @project = Project.find(params[:id])
     @project.destroy
     flash[:notice] = "Project has been destroyed."
     redirect_to projects_path
@@ -52,6 +49,15 @@ class ProjectsController < ApplicationController
 
   def project_params
     params.require(:project).permit(:name, :description) 
+  end
+
+  def set_project
+    begin
+      @project = Project.find(params[:id]) 
+    rescue ActiveRecord::RecordNotFound 
+      flash[:alert] = "The project you were looking for could not be found." 
+      redirect_to projects_path 
+    end
   end
 
 
