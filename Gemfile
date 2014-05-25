1625fe74c5b02a1135b069540f150d1b87d62886
Andrew <arhautau@gmail.com>
2014-05-24 17:25:02 -0700

    remove nyan cat, for now

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'pry-rails'
-gem 'nyan-cat-formatter'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.1'
 # Use postgresql as the database for Active Record
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,9 +25,6 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
-  config.color_enabled = true
-  config.formatter = 'NyanCatWideFormatter' 
-  config.warnings = false
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
